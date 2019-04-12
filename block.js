9bf60b53e12a2fafa6e69d11cc55029a229466f6
igmerwina <igmerwina@gmail.com>
2019-04-12 21:27:51 +0700

    create genesis block

--- block.js
+++ block.js
@@ -11,10 +11,20 @@ class Block {
     toString(){
         return `Block - 
             Timestamp: ${this.timestamp}
-            Last Hash: ${this.lastHash.substring(0, 10)}
-            Hash     : ${this.hash.substring(0, 10)}
+            Last Hash: ${this.lastHash.substring(0, 10) + '...'}
+            Hash     : ${this.hash.substring(0, 10) + '...'}
             Data     : ${this.data}
         `
+    }
+
+    // genesis block
+    static genesis(){
+        return new this(
+            'Genesis Timestamp', 
+            '-----', 
+            '5feceb66ffc86f38d952786c6d696c79c2dbc239dd4e91b46729d73a27fb57e9', 
+            []
+        )
     }
 }
 
--- dev-test.js
+++ dev-test.js
@@ -3,3 +3,6 @@ const Block = require('./block')
 const block = new Block('foo', 'bar', 'zoo', 'baz')
 console.log(block.toString())
 
+// Block karena manggil dari class
+console.log(Block.genesis().toString())
+
