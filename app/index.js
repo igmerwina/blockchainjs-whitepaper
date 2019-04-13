15e3947544cf3373f0c068d6b944b1376b328d38
igmerwina <igmerwina@gmail.com>
2019-04-13 19:22:43 +0700

    add: mine block post request

--- app/index.js
+++ app/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const bp = require('body-parser')
 const Blockchain = require('../blockchain')
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001 // biar bisa manggil banyak port
@@ -6,8 +7,17 @@ const HTTP_PORT = process.env.HTTP_PORT || 3001 // biar bisa manggil banyak port
 const app = express()
 const bc = new Blockchain()
 
+app.use(bp.json()) // allow us to receive json on post request
+
 app.get('/blocks', (req,res) => {
   res.json(bc.chain)
+})
+
+app.post('/mine', (req, res) => {
+  const block = bc.addBlock(req.body.data)
+  console.log(`New block added: ${block.toString()}`)
+
+  res.redirect('/blocks')
 })
 
 app.listen(HTTP_PORT, () => console.log(`jalan di http://localhost:${HTTP_PORT}`))
