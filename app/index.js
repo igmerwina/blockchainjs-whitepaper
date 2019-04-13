0156f5cc111c52f68065cec9f25200b279229419
igmerwina <igmerwina@gmail.com>
2019-04-13 20:23:27 +0700

    add: connect to blockchain peers

--- app/index.js
+++ app/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const bp = require('body-parser')
 const Blockchain = require('../blockchain')
+const p2pServer = require('./p2p-server') 
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001 // biar bisa manggil banyak port
 
 const app = express()
 const bc = new Blockchain()
+const p2p = new p2pServer(bc)
 
 app.use(bp.json()) // allow us to receive json on post request
 
@@ -21,3 +23,4 @@ app.post('/mine', (req, res) => {
 })
 
 app.listen(HTTP_PORT, () => console.log(`jalan di http://localhost:${HTTP_PORT}`))
+p2p.listen()
--- app/p2p-server.js
+++ app/p2p-server.js
@@ -5,7 +5,7 @@ const ws = require('ws') //import websocket
 const P2P_PORT = process.env.P2P_PORT || 5001
 const peers = process.env.PEERS ? process.env.PEERS.split(',') : []
 
-class p2pServer(){
+class p2pServer {
   constructor(blockchain){
     this.blockchain = blockchain
     this.socket = []
@@ -14,11 +14,27 @@ class p2pServer(){
   listen(){
     const server = new ws.Server({ port: P2P_PORT })
     server.on('connection', socket => this.connectSocket(socket))
-    console.log(`Listening for peer-to-peer conn on ${P2P_PORT}`)
+    
+    this.connectToPeers()
+
+    console.log(`Listening for peer-to-peer conn on http://localhost/${P2P_PORT}`)
   }
+
+
+  connectToPeers(){
+    peers.forEach(peer => {
+      const socket = new ws(peer)
+
+      socket.on('open', () => this.connectSocket(socket))
+    })
+  }
+
 
   connectSocket(socket){
     this.socket.push(socket)
     console.log('Socket connected')
   }
 }
+
+
+module.exports = p2pServer
