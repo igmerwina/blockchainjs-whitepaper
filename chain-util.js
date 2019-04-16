eee266c222b1210bd1801c5aabd17b406088ccf0
igmerwina <igmerwina@gmail.com>
2019-04-16 15:29:53 +0700

    add: transaction class & method

--- chain-util.js
+++ chain-util.js
@@ -1,12 +1,17 @@
 // berisi file untuk generate wallet key, unique hasesh etc
 
 const EC = require('elliptic').ec // import lib elictip curve algo  
+const uuidV1 = require('uuid/v1')
 const ec = new EC('secp256k1') // implementasi jenis algo elictip pada bitcoin
 
  
 class ChainUtil{
     static genKeyPair(){
         return ec.genKeyPair()
+    }
+
+    static id(){
+        return uuidV1()
     }
 }
 
---
+++ wallet /transaction.js
@@ -0,0 +1,37 @@
+// logika proses transaksi ada di sini
+// kaya transfer uang, dsb 
+
+const ChainUtil = require('../chain-util')
+
+class Transaction{
+    constructor(){
+        this.id = ChainUtil.id()
+        this.input = null 
+        this.outputs = []
+    }
+
+    static newTransaction(senderWallet, recipient, amount){
+        const transaction = new this()
+        
+        // check the balance di wallet bisa gak dipake buat transaksi
+        if(amount > senderWallet.balance){
+            console.log(`Amount: ${amount} exceeds balance`)
+            return
+        }
+        // outputs yang dicatet dari proses transaksi: 
+        transaction.outputs.push(...[
+            { 
+                amount: senderWallet.balance - amount,
+                address: senderWallet.publicKey
+            }, 
+            {
+                amount, address: recipient
+            }
+        ])
+
+        return transaction
+    }
+}
+
+
+module.exports = Transaction
