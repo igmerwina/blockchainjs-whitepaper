6368ffba4dd4e45e49fd3f69ee1eb6075f30a673
igmerwina <igmerwina@gmail.com>
2019-04-17 13:12:12 +0700

    add: Miner class & constructor

---
+++ app/miner.js
@@ -0,0 +1,20 @@
+class Miner {
+    constructor(blockchain, transactionPool, wallet, p2pServer){
+        this.blockchain = blockchain
+        this.transactionPool = transactionPool
+        this.wallet = wallet
+        this.p2pServer = p2pServer
+    }
+
+    mine(){
+        const validTransaction = this.transactionPool.validTransactions(){
+        // include reward for the miner
+        // create a block consisting of valid transactions
+        // sync the chain in p2p
+        // clear transaction pool
+        // broadcast to every miner to clear their transaction pools
+        }
+    }
+}
+
+module.exports = Miner
